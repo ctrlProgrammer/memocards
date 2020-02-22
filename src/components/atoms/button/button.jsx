66c15b09519e31bd10b3e45358510ff20b8e540e
CtrlProgrammer <sebastianto1999@gmail.com>
2020-02-22 06:44:30 -0500

    Change button component to functionality component

--- src/components/atoms/button/button.jsx
+++ src/components/atoms/button/button.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 
-export default class Button extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const Button = props => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text !== undefined ? props.text : "notext"}
+    </button>
+  );
+};
 
-    this.defProps = {
-      text: "notext"
-    };
-  }
-
-  propertyValidation() {
-    /**
-     * Validacion de propiedades
-     */
-    this.propsKeys = Object.keys(this.defProps);
-    this.propsKeys.forEach(key => {
-      if (this.props[key] === undefined)
-        console.log("No existe la propiedad: " + key);
-    });
-  }
-
-  render() {
-    return (
-      <button onClick={this.props.onClick}>
-        {this.props.text !== undefined ? this.props.text : this.defProps.text}
-      </button>
-    );
-  }
-}
+export default Button;
