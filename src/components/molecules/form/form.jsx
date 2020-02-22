7393c5f3f37f1d7dabcf37761e94b0a801a01bb4
CtrlProgrammer <sebastianto1999@gmail.com>
2020-02-22 07:49:30 -0500

    update form to minimal component

--- src/components/molecules/form/form.jsx
+++ src/components/molecules/form/form.jsx
@@ -1,66 +1,27 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
-
-import { connect } from "react-redux";
-import setPlayer from "./../../../redux/actions/player/setPlayer.js";
-import getPlayer from "./../../../redux/actions/player/getPlayer.js";
-import changeState from "./../../../redux/actions/game/changeState.js";
 
 import Input from "./../../atoms/input";
 import Button from "./../../atoms/button";
 
-class ConnectedForm extends React.Component {
-  constructor(props) {
-    super(props);
+export default class Form extends React.Component {
+  state = {
+    input: ""
+  };
 
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-
-    this.state = {
-      input: "",
-      player: ""
-    };
-  }
-
-  handleInput(e) {
-    this.setState({ input: e.target.value });
-  }
-
-  handleSubmit() {
-    const { input } = this.state;
-    this.props.setPlayer(input);
-    this.props.getPlayer();
-    this.props.changeState(1);
-  }
+  handleInput = e => this.setState({ input: e.currentTarget.value });
 
   render() {
-    if (this.props.game === 1) {
-      return <Redirect to="/game" />;
-    } else {
-      return (
-        <form className="form" onSubmit={e => this.props.handleInput(e)}>
-          <Input
-            onChange={this.handleInput}
-            name={this.props.input.name}
-            type={this.props.input.type}
-            placeholder={this.props.input.placeholder}
-            value={this.state.input}
-          />
-          <Button text={this.props.button.text} />
-        </form>
-      );
-    }
+    return (
+      <form className="form" onSubmit={e => this.props.handleInput(e)}>
+        <Input
+          onChange={this.handleInput}
+          name={this.props.input.name}
+          type={this.props.input.type}
+          placeholder={this.props.input.placeholder}
+          value={this.state.input}
+        />
+        <Button text={this.props.button.text} />
+      </form>
+    );
   }
 }
-
-const mapStateToProps = state => {
-  return { player: state.player, game: state.game };
-};
-
-const mapDispatchToProps = {
-  setPlayer,
-  getPlayer,
-  changeState
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ConnectedForm);
