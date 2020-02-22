a94922d19f94023294fc238698975c667b528153
CtrlProgrammer <sebastianto1999@gmail.com>
2020-02-22 08:04:04 -0500

    Include game state validations on game page

--- src/components/pages/game.jsx
+++ src/components/pages/game.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
+
+//Redux
 import { connect } from "react-redux";
 import setCards from "../../redux/actions/card/setCards";
+import changeState from "../../redux/actions/game/changeState";
 
 import Title from "../atoms/title";
 import Board from "../organisms/board";
@@ -42,23 +46,31 @@ class Game extends React.Component {
   }
 
   render() {
-    return (
-      <div className="game">
-        <Title title={"Juega " + this.props.player + "!"} />
-        <Board cards={this.chaosCards()} />
-      </div>
-    );
+    if (this.props.game === 0) {
+      return <Redirect to="/" />;
+    } else if (this.props.game === 2) {
+      return <Redirect to="/end" />;
+    } else {
+      return (
+        <div className="game">
+          <Title title={"Juega " + this.props.player + "!"} />
+          <Board cards={this.chaosCards()} />
+        </div>
+      );
+    }
   }
 }
 
 const mapStateToProps = state => {
   return {
-    player: state.player
+    player: state.player,
+    game: state.game
   };
 };
 
 const mapDispatchToProps = {
-  setCards
+  setCards,
+  changeState
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Game);
